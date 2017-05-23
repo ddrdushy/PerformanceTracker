cdad1c3eb77a41026be1aa982302b69d15bf1a62
ddrdushy <dushy2009@gmail.com>
2017-05-24 00:50:40 +0530

    updated the api

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,16 @@ const testFolder = path.join(__dirname+'/../data/');
 /* GET users listing. */
 router.get('/files', function(req, res, next) {
     fs.readdir(testFolder, (err, files) => {
-        res.json(files);
+        var data = {};
+        files.map((e)=>{
+            if(data.hasOwnProperty(e.split('_')[0]))
+                data[e.split('_')[0]].push(e.split('_')[1].replace('.md',''));
+            else{
+                data[e.split('_')[0]] = [];
+                data[e.split('_')[0]].push(e.split('_')[1].replace('.md',''));
+            }
+        });
+        res.json(data);
     });
 });
 
