44fc08565e41945af52d5909a89a55ce12c537f1
ddrdushy <dushy2009@gmail.com>
2017-05-23 23:21:19 +0530

    created the api for filename

--- routes/api.js
+++ routes/api.js
@@ -7,11 +7,17 @@ const testFolder = path.join(__dirname+'/../data/');
 
 /* GET users listing. */
 router.get('/files', function(req, res, next) {
-  //var files =[];
     fs.readdir(testFolder, (err, files) => {
         res.json(files);
     });
+});
 
+router.get('/files/:filename', function(req, res, next) {
+  console.log(path.join(testFolder,req.params.filename));
+  var ff= path.join(testFolder,req.params.filename);
+    fs.readFile(ff,'utf8',(err,data)=>{
+      res.json({"data":data});
+    });
 });
 
 module.exports = router;
