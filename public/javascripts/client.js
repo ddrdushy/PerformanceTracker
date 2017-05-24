625713dca255ed8b733575ff1f5f45d7fb346d25
ddrdushy <dushy2009@gmail.com>
2017-05-24 12:51:16 +0530

    updated the validations and loader

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,8 @@
 var serverData;
 $(document).ready(()=>{
+    $('#loading-image').show();
+    $('#month').prop('disabled',true);
+    $('#load').prop('disabled',true);
     $.ajax({
         type: 'GET',
         url: './api/files'
@@ -7,7 +10,9 @@ $(document).ready(()=>{
     .done((data)=>{
         serverData =  data;
         loadYears(data);
+        $('#loading-image').hide();
     });
+
 
     $('#year').on('change',()=>{
         console.log(serverData);
@@ -37,15 +42,18 @@ function loadYears(years){
 }
 
 function loadMonths(months) {
-    console.log(months);
     if(!!months){
+        $('#month').prop('disabled')?$('#month').prop('disabled',false):$('#month').prop('disabled',true);
         var html = '<option>Select</option><option>';
         html+=months.join('</option><option>');
         html+='</option>'
         $('#month').html(html);
+        $('#load').prop('disabled',false);
         return;
     }
+
     $('#month').html('<option>Select</option>');
     $('#month').prop('disabled',true);
+    $('#load').prop('disabled',true);
     return;
 }
